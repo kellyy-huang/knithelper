49f4fa71cc61124b0ce1dc8b7e93a7c26399e27d
kelly <kyhuang919@gmail.com>
2023-07-21 16:31:40 -0700

    add counter buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [counter, setCounter] = React.useState(0);
   React.useEffect(() => {
     fetch("/home")
     .then((res) => res.json())
-    .then((data) => setData(data.message));
+    .then((data) => counter);
   })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{counter}</p>
+        <Button variant="contained" onClick={() => setCounter(counter + 1)}>+</Button>
+        <br></br>
+        <Button variant="contained" onClick={() => setCounter(counter - 1)}>-</Button>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from "express";
-import path from "path";
+
+const message = "counter";
+
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.static("build"));
 app.get("/home", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: message });
   });
-
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
